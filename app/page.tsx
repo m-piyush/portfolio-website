c956ee689a75f5300159271c1a91aa28c1113228
m-piyush <piyushmalviya1969@gmail.com>
2024-03-29 00:29:15 +0530

    main-branch

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
 import About from "@/components/about";
-import Contact from "@/components/contact";
+import Interest from "@/components/interest";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
+import Education from "@/components/education";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
-      <SectionDivider />
+      {/* <SectionDivider /> */}
       <About />
-      <Projects />
-      <Skills />
       <Experience />
-      <Contact />
+      <Projects />
+      <Education />
+      <Skills />
+      <Interest />
     </main>
   );
 }
